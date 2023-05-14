4daa88672237de765b1eba8b5a9a52ad3942b8ac
sagemodeninja <contact@garyantier.com>
2023-05-14 11:57:27 +0800

    feat: base

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,36 @@
+import { colorSchemes } from '../core/dashboard-item';
 import { BarGraph } from './bar-graph';
 
 export { BarGraph };
+
+let colorScheme: string;
+let toggleColorSchemeBtn: HTMLButtonElement;
+let barGraph: BarGraph;
+
+document.addEventListener('DOMContentLoaded', () => {
+    toggleColorSchemeBtn = document.getElementById('toggle_color_scheme') as HTMLButtonElement;
+    barGraph = document.getElementById('bar_graph') as BarGraph;
+
+    toggleColorSchemeBtn.addEventListener('click', () => {
+        const isDark = colorScheme != 'Dark';
+
+        colorScheme = !isDark ? 'Light' : 'Dark';
+        toggleColorScheme();
+    });
+
+    colorScheme = resolveColorScheme();
+    toggleColorScheme();
+});
+
+function resolveColorScheme() {
+    const media = '(prefers-color-scheme: dark)';
+    const isDark = window.matchMedia && window.matchMedia(media).matches;
+
+    return !isDark ? 'Light' : 'Dark';
+}
+
+function toggleColorScheme() {
+    barGraph.preferredColorScheme = colorScheme.toLowerCase() as colorSchemes;
+    toggleColorSchemeBtn.innerText = `Toggle: ${colorScheme}`;
+    document.body.classList.toggle('dark', colorScheme == 'Dark');
+}
--- src/core/dashboard-item.ts
+++ src/core/dashboard-item.ts
@@ -13,13 +13,46 @@ export type colorSchemes = 'inherit' | 'light' | 'dark';
 export class DashboardItem extends LitElement {
     static styles = css`
         :host {
-            --color: red;
+            --base-border: rgba(0 0 0 / 5.78%);
+            --base-fill-rest: #FDFDFD;
+            --base-fill-hover: #FFFF;
+            --base-shadow-rest: rgb(0 0 0 / 4%);
+            --base-shadow-hover: rgb(0 0 0 / 10%);
             color-scheme: light;
         }
 
         :host([data-color-scheme=dark]) {
-            --color: green;
+            --base-border: rgba(0 0 0 / 10%);
+            --base-fill-rest: #333333;
+            --base-fill-hover: #373737;
+            --base-shadow-rest: rgb(0 0 0 / 13%);
+            --base-shadow-hover: rgb(0 0 0 / 26%);
             color-scheme: dark;
+        }
+
+        :host {
+            display: flex;
+            height: min-content;
+            width: min-content;
+        }
+
+        .base {
+            background-color: var(--base-fill-rest);
+            border: 1px inset var(--base-border);
+            border-radius: 7px;
+            box-shadow: 0px 2px 4px 0px var(--base-shadow-rest);
+            height: 250px;
+            position: relative;
+            width: 250px;
+        }
+
+        :host(:hover) .base {
+            background-color: var(--base-fill-hover);
+            box-shadow: 0px 2px 4px 0px var(--base-shadow-hover);
+        }
+
+        :host(:active) .base {
+            box-shadow: none;
         }
     ` as CSSResultGroup;
 
@@ -33,7 +66,7 @@ export class DashboardItem extends LitElement {
 
     public render() {
         return html`
-        <div>
+        <div class="base">
             ${this.renderContent()}
         </div>
         `;
